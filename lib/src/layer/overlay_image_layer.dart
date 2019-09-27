4b1d796e47e9718b35f1f896f96b929aad4742dc
David <david@knight.fm>
2019-09-27 09:54:27 -0700

    overlay images

--- lib/src/layer/overlay_image_layer.dart
+++ lib/src/layer/overlay_image_layer.dart
@@ -1,11 +1,5 @@
 import 'dart:async';
 import 'dart:ui';
-import 'dart:ui' as ui;
-
-import 'package:flutter/rendering.dart' as img;
-import 'package:flutter/services.dart' as img;
-import 'package:flutter/widgets.dart' as img;
-import 'package:flutter/painting.dart' as img;
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -18,30 +12,15 @@ class OverlayImageLayerOptions extends LayerOptions {
 }
 
 class OverlayImage {
-  final img.ImageProvider imageProvider;
-  final double opacity;
   final LatLngBounds bounds;
-  final List<Offset> offsets = [];
-  ui.Image image;
+  final ImageProvider imageProvider;
+  final double opacity;
 
   OverlayImage({
     this.bounds,
     this.imageProvider,
     this.opacity = 1.0,
   });
-}
-
-Future<ui.Image> _loadImage(img.ImageProvider imageProvider) async {
-  img.ImageStream stream = imageProvider.resolve(img.ImageConfiguration.empty);
-  Completer<ui.Image> completer = new Completer<ui.Image>();
-  void listener(img.ImageInfo frame, bool synchronousCall) {
-    final ui.Image image = frame.image;
-    completer.complete(image);
-    stream.removeListener(listener);
-  }
-
-  stream.addListener(listener);
-  return completer.future;
 }
 
 class OverlayImageLayer extends StatelessWidget {
@@ -51,79 +30,47 @@ class OverlayImageLayer extends StatelessWidget {
 
   OverlayImageLayer(this.overlayImageOpts, this.map, this.stream);
 
+  @override
   Widget build(BuildContext context) {
-    return new LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints bc) {
-        final size = new Size(bc.maxWidth, bc.maxHeight);
-        return _build(context, size);
-      },
-    );
-  }
-
-  Widget _build(BuildContext context, Size size) {
-    return new StreamBuilder<int>(
-      stream: stream, // a Stream<int> or null
+    return StreamBuilder<int>(
+      stream: stream,
       builder: (BuildContext context, _) {
-        for (var overlayImageOpt in overlayImageOpts.overlayImages) {
-          overlayImageOpt.offsets.clear();
-          var pos1 = map.project(overlayImageOpt.bounds.northWest);
-          pos1 = pos1.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) -
-              map.getPixelOrigin();
-          var pos2 = map.project(overlayImageOpt.bounds.southEast);
-          pos2 = pos2.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) -
-              map.getPixelOrigin();
-
-          overlayImageOpt.offsets
-              .add(new Offset(pos1.x.toDouble(), pos1.y.toDouble()));
-          overlayImageOpt.offsets
-              .add(new Offset(pos2.x.toDouble(), pos2.y.toDouble()));
-          _loadImage(overlayImageOpt.imageProvider).then((image) {
-            overlayImageOpt.image = image;
-          });
-        }
-
-        var overlayImages = <Widget>[];
-        for (var overlayImageOpt in this.overlayImageOpts.overlayImages) {
-          overlayImages.add(
-            new CustomPaint(
-              painter: new OverlayImagePainter(overlayImageOpt),
-              size: size,
-            ),
-          );
-        }
-
-        return new Container(
-          child: new Stack(
-            children: overlayImages,
+        final zoomScale = map.getZoomScale(map.zoom, map.zoom);
+        final pixelOrigin = map.getPixelOrigin();
+        return ClipRect(
+          child: Stack(
+            children: <Widget>[
+              for (var overlayImage in overlayImageOpts.overlayImages)
+                Builder(
+                  builder: (BuildContext context) {
+                    final upperLeftPixel = map
+                            .project(overlayImage.bounds.northWest)
+                            .multiplyBy(zoomScale) -
+                        pixelOrigin;
+                    final bottomRightPixel = map
+                            .project(overlayImage.bounds.southEast)
+                            .multiplyBy(zoomScale) -
+                        pixelOrigin;
+                    return Positioned(
+                      left: upperLeftPixel.x.toDouble(),
+                      top: upperLeftPixel.y.toDouble(),
+                      width: (bottomRightPixel.x - upperLeftPixel.x).toDouble(),
+                      height:
+                          (bottomRightPixel.y - upperLeftPixel.y).toDouble(),
+                      child: Image(
+                        image: overlayImage.imageProvider,
+                        fit: BoxFit.fill,
+                        color:
+                            Color.fromRGBO(255, 255, 255, overlayImage.opacity),
+                        colorBlendMode: BlendMode.dstATop,
+                      ),
+                    );
+                  },
+                ),
+            ],
           ),
         );
       },
     );
   }
-}
-
-class OverlayImagePainter extends CustomPainter {
-  final OverlayImage overlayImageOpt;
-  BoxFit boxfit = BoxFit.fitWidth;
-  OverlayImagePainter(this.overlayImageOpt);
-  @override
-  void paint(Canvas canvas, Size size) {
-    final rect = Offset.zero & size;
-    canvas.clipRect(rect);
-    final paint = new Paint()
-      ..color = Color.fromRGBO(255, 255, 255, overlayImageOpt.opacity);
-
-    final Size imageSize = new Size(overlayImageOpt.image.width.toDouble(),
-        overlayImageOpt.image.height.toDouble());
-    final Rect inputSubrect = Offset.zero & imageSize;
-
-    canvas.drawImageRect(
-        overlayImageOpt.image,
-        inputSubrect,
-        Rect.fromPoints(overlayImageOpt.offsets[0], overlayImageOpt.offsets[1]),
-        paint);
-  }
-
-  @override
-  bool shouldRepaint(OverlayImagePainter other) => false;
 }
